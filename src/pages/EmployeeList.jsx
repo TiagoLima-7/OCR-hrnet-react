419358ee6c8a6e10666303e7c5e812ed0eb0fe21
Tiago Lima <lima_tiago@hotmail.com>
2026-09-12 11:51:12 +0200

    feat: connect EmployeeList to Redux store and render DataTable

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectEmployees } from "../store/employeesSlice";
 import { DataTable } from "hrnet-datatable";
 
+const columns = [
+  { key: "firstName", label: "First Name" },
+  { key: "lastName", label: "Last Name" },
+  { key: "dateOfBirth", label: "Date Of Birth" },
+  { key: "startDate", label: "Start Date" },
+  { key: "street", label: "Street" },
+  { key: "city", label: "City" },
+  { key: "state", label: "State" },
+  { key: "zipCode", label: "Zip Code" },
+  { key: "department", label: "Department" },
+];
+
 export default function EmployeeList() {
+  const employees = useSelector(selectEmployees);
+
   return (
     <div className="container">
       <h1>Current employees</h1>
-      <DataTable />
+      <DataTable data={employees} columns={columns} />
       <Link to="/create">Home</Link>
     </div>
   );
